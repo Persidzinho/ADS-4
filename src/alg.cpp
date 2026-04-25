7b51b229c01e2a0c931d2b4b4887f5d09e3ec782
Никита Чванов <143615653+Persidzinho@users.noreply.github.com>
2026-04-25 22:09:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,58 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len; ++i) {
+    for (int j = i + 1; j < len; ++j) {
+      if (arr[i] + arr[j] == value) {
+        ++count;
+      }
+    }
+  }
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  int left = 0;
+  int right = len - 1;
+  while (left < right) {
+    int sum = arr[left] + arr[right];
+    if (sum == value) {
+      int lval = arr[left];
+      int rval = arr[right];
+      int cntL = 0, cntR = 0;
+      while (left < len && arr[left] == lval) { ++cntL; ++left; }
+      while (right >= 0 && arr[right] == rval) { ++cntR; --right; }
+      if (lval == rval)
+        count += cntL * (cntL - 1) / 2;
+      else
+        count += cntL * cntR;
+    } else if (sum < value) {
+      ++left;
+    } else {
+      --right;
+    }
+  }
+  return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len; ++i) {
+    int target = value - arr[i];
+    int lb = i + 1, ub = len;
+    while (lb < ub) {
+      int mid = lb + (ub - lb) / 2;
+      if (arr[mid] < target) lb = mid + 1;
+      else ub = mid;
+    }
+    int low = lb;
+    lb = i + 1; ub = len;
+    while (lb < ub) {
+      int mid = lb + (ub - lb) / 2;
+      if (arr[mid] <= target) lb = mid + 1;
+      else ub = mid;
+    }
+    int high = lb;
+    count += (high - low);
+  }
+  return count;
 }
