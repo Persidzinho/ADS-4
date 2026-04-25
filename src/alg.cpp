3901b01f6e095905e154d2c30e22f3fc76ccb973
Никита Чванов <143615653+Persidzinho@users.noreply.github.com>
2026-04-25 22:04:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,50 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len; ++i) {
+    for (int j = i + 1; j < len; ++j) {
+      if (arr[i] + arr[j] == value) {
+        ++count;
+      }
+    }
+  }
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len; ++i) {
+    for (int j = len - 1; j > i; --j) {
+      int sum = arr[i] + arr[j];
+      if (sum == value) {
+        ++count;
+      } else if (sum < value) {
+        break;
+      }
+    }
+  }
+  return count;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len; ++i) {
+    int target = value - arr[i];
+    int left = i + 1;
+    int right = len;
+    while (left < right) {
+      int mid = left + (right - left) / 2;
+      if (arr[mid] < target) left = mid + 1;
+      else right = mid;
+    }
+    int lb = left;
+    left = i + 1;
+    right = len;
+    while (left < right) {
+      int mid = left + (right - left) / 2;
+      if (arr[mid] <= target) left = mid + 1;
+      else right = mid;
+    }
+    int ub = left;
+    count += (ub - lb);
+  }
+  return count;
 }
