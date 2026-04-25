7e1eda5a5d45088e4109b2f697cbfc3b13fbe37e
Никита Чванов <143615653+Persidzinho@users.noreply.github.com>
2026-04-25 22:06:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,14 +12,24 @@ int countPairs1(int *arr, int len, int value) {
 }
 int countPairs2(int *arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len; ++i) {
-    for (int j = len - 1; j > i; --j) {
-      int sum = arr[i] + arr[j];
-      if (sum == value) {
-        ++count;
-      } else if (sum < value) {
-        break;
-      }
+  int left = 0;
+  int right = len - 1;
+  while (left < right) {
+    int sum = arr[left] + arr[right];
+    if (sum == value) {
+      int lval = arr[left];
+      int rval = arr[right];
+      int cntL = 0, cntR = 0;
+      while (left < len && arr[left] == lval) { ++cntL; ++left; }
+      while (right >= 0 && arr[right] == rval) { ++cntR; --right; }
+      if (lval == rval)
+        count += cntL * (cntL - 1) / 2;
+      else
+        count += cntL * cntR;
+    } else if (sum < value) {
+      ++left;
+    } else {
+      --right;
     }
   }
   return count;
@@ -28,23 +38,21 @@ int countPairs3(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
     int target = value - arr[i];
-    int left = i + 1;
-    int right = len;
-    while (left < right) {
-      int mid = left + (right - left) / 2;
-      if (arr[mid] < target) left = mid + 1;
-      else right = mid;
+    int lb = i + 1, ub = len;
+    while (lb < ub) {
+      int mid = lb + (ub - lb) / 2;
+      if (arr[mid] < target) lb = mid + 1;
+      else ub = mid;
     }
-    int lb = left;
-    left = i + 1;
-    right = len;
-    while (left < right) {
-      int mid = left + (right - left) / 2;
-      if (arr[mid] <= target) left = mid + 1;
-      else right = mid;
+    int low = lb;
+    lb = i + 1; ub = len;
+    while (lb < ub) {
+      int mid = lb + (ub - lb) / 2;
+      if (arr[mid] <= target) lb = mid + 1;
+      else ub = mid;
     }
-    int ub = left;
-    count += (ub - lb);
+    int high = lb;
+    count += (high - low);
   }
   return count;
 }
